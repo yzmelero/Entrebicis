8d5c57c223692a2a7478c582c3d53693fc339803
Yaiza Melero <ymeleromaldonado@gmail.com>
2025-03-20 13:57:36 +0100

    Backend

---
+++ BackEnd/src/main/java/cat/copernic/ymelero/EntreBicis/EntreBicisApplication.java
@@ -0,0 +1,13 @@
+package cat.copernic.ymelero.EntreBicis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntreBicisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntreBicisApplication.class, args);
+	}
+
+}
