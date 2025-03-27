6ad363e8a034e0149c8fd12875557ec8b71b3e1b
Yaiza Melero <ymeleromaldonado@gmail.com>
2025-03-27 11:50:08 +0100

    Logout funciona

---
+++ BackEnd/src/main/java/cat/copernic/ymelero/EntreBicis/controller/web/LogoutController.java
@@ -0,0 +1,31 @@
+package cat.copernic.ymelero.entrebicis.controller.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping("/logout")
+public class LogoutController {
+
+    private static final Logger log = LoggerFactory.getLogger(LogoutController.class);
+
+    @GetMapping
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+            log.info("Sessió finalitzada correctament per a l'usuari: {}", auth.getName());
+        }
+        return "redirect:/login?logout=true";
+    }
+}
