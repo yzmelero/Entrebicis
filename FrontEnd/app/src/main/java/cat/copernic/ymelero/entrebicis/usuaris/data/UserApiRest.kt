8cddbd13f2084efd4730d3280430c1ccc079dace
Yaiza Melero <ymeleromaldonado@gmail.com>
2025-04-10 22:33:52 +0200

    backend

--- FrontEnd/app/src/main/java/cat/copernic/ymelero/entrebicis/usuaris/data/UserApiRest.kt
+++ FrontEnd/app/src/main/java/cat/copernic/ymelero/entrebicis/usuaris/data/UserApiRest.kt
@@ -1,5 +1,6 @@
 package cat.copernic.ymelero.entrebicis.usuaris.data
 
 interface UserApiRest {
-
+    @POST("login/verify")
+    suspend fun loginUser(@Body loginRequest: LoginRequest): Response<LoginResponse>
 }
