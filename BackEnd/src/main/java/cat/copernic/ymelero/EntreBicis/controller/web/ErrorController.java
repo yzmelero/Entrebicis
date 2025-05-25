4760b4cb365f4baca59a22c79201f36e1773d9f3
Yaiza Melero <ymeleromaldonado@gmail.com>
2025-05-25 18:16:16 +0200

    Solucionada la fluidesa

--- BackEnd/src/main/java/cat/copernic/ymelero/EntreBicis/controller/web/ErrorController.java
+++ BackEnd/src/main/java/cat/copernic/ymelero/EntreBicis/controller/web/ErrorController.java
@@ -3,6 +3,7 @@ package cat.copernic.ymelero.entrebicis.controller.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -16,8 +17,10 @@ public class ErrorController {
      * @return Vista d'error.
      */
     @GetMapping("/error")
-    public String paginaError() {
+    public String paginaError(Model model) {
         log.warn("S'ha redirigit a la pàgina d'error genèrica");
+        model.addAttribute("errorMessage",
+                "Error de connexió amb la base de dades o el servidor. Comprova que tot estigui encès.");
         return "error";
     }
 }
